befabf9ed7f30b17fa75ca0c89b4e1085175f171
Pranita Gughane <69386793+pranita1999@users.noreply.github.com>
2023-09-11 13:35:29 +0530

    Update before_install.sh

--- mydjango/scripts/before_install.sh
+++ mydjango/scripts/before_install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Stop and remove the previous Docker container (if it exists)
-if docker ps -a | grep -q "myapp_container"; then
-  docker stop myapp_container
-  docker rm myapp_container
-fi
-
 # Pull the new Docker image from your container registry (e.g., Amazon ECR)
 docker pull public.ecr.aws/c0k5f5m3/mydjango:latest
 
